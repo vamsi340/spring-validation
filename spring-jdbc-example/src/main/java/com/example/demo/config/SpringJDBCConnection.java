246359356f82a9cedb9315b4a293b15022661937
vamsi R <vamsi.r@scriptbees.com>
2020-07-09 08:11:13 +0530

    JdbcTemplate

--- spring-jdbc-example/src/main/java/com/example/demo/config/SpringJDBCConnection.java
+++ spring-jdbc-example/src/main/java/com/example/demo/config/SpringJDBCConnection.java
@@ -5,7 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
@@ -17,8 +17,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @ComponentScan(basePackages = { "com.example.demo.dao" })
 public class SpringJDBCConnection {
 	@Bean
-	public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
-		return new NamedParameterJdbcTemplate(dataSource());
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
 	}
 
 	@Bean
